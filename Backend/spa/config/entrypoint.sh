04634080ad3b09d7cbb28d2af93dea05150e8ecc
Basile Grosjean <bgrosjea@k1r4p6.42mulhouse.fr>
2025-02-14 11:19:27 +0100

    bgrosjea 2025-02-14 11:19:26 2025-02-14 11:19:26

--- Backend/spa/config/entrypoint.sh
+++ Backend/spa/config/entrypoint.sh
@@ -6,5 +6,11 @@ npm start &
 
 while inotifywait -r -e modify,create,delete /usr/src/app/Frontend; do
     echo "Changement détecté ! Redémarrage du service..."
-    npx tsc
-done
+    npx tsc  # Ou relancer le processus concerné
+done &
+
+while inotifywait -r -e modify,create,delete /usr/src/app/Backend; do
+    echo "Changement détecté ! Redémarrage du serveur..."
+	kill $(pgrep -f "node")
+    npm start & # Ou relancer le processus concerné
+done &
