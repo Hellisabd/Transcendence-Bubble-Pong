65f6a860d42178e85ea0e96b7f5bd12cbb259fcb
Basile Grosjean <bgrosjea@k1r4p6.42mulhouse.fr>
2025-02-14 12:16:20 +0100

    bgrosjea 2025-02-14 12:16:19 volumes pong et spa

--- Backend/spa/config/entrypoint.sh
+++ Backend/spa/config/entrypoint.sh
@@ -13,4 +13,4 @@ while inotifywait -r -e modify,create,delete /usr/src/app/Backend; do
     echo "Changement détecté ! Redémarrage du serveur..."
 	kill $(pgrep -f "node")
     npm start & # Ou relancer le processus concerné
-done &
+done
