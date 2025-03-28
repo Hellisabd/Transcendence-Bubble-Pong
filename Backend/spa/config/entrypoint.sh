b42670c117a1fe8bf2f8b31888f7fc790e8fe863
allan hans <allan.hans68350@gmail.com>
2025-03-28 13:17:19 +0100

    scroll bar

--- Backend/spa/config/entrypoint.sh
+++ Backend/spa/config/entrypoint.sh
@@ -5,6 +5,10 @@ npx tsc
 chown -R root:root /usr/src/app/Frontend
 npm start &
 
+while inotifywait -r -e modify,create,delete /usr/src/app/Frontend; do
+    echo "Changement détecté ! Redémarrage du service..."
+    npx tailwindcss -i ./Frontend/css/style.css -o ./Frontend/css/output.css
+done &
 
 while inotifywait -r -e modify,create,delete /usr/src/app/Frontend/ts; do
     echo "Changement détecté ! Redémarrage du service..."
@@ -16,5 +20,3 @@ while inotifywait -r -e modify,create,delete /usr/src/app/Backend; do
 	kill $(pgrep -f "node")
     npm start & # Ou relancer le processus concerné
 done
-
-# npx tailwindcss -i ./Frontend/css/style.css -o ./Frontend/css/output.css --watch
