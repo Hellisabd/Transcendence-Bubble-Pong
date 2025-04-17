69509aab8f9c5fe602f0d482d44753518b7964b8
basile <firestone945.ow@gmail.com>
2025-04-17 13:46:02 +0200

    bgrosjea 2025-04-17 13:45:24 2025-04-17 13:45:24

--- Backend/spa/config/entrypoint.sh
+++ Backend/spa/config/entrypoint.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
 npx tsc
-chown -R root:root /usr/src/app/Frontend
+chown -R spa:spa /usr/src/app/Frontend
 
 npx tailwindcss -i ./Frontend/css/style.css -o ./Frontend/css/output.css
-npm start &
 
-while inotifywait -r -e modify,create,delete /usr/src/app/Frontend/templates; do
-    echo "Changement détecté ! Redémarrage du service..."
-    npx tailwindcss -i ./Frontend/css/style.css -o ./Frontend/css/output.css
-    npx tsc  # Ou relancer le processus concerné
-done &
+# Prod
+npm start 
 
-while inotifywait -r -e modify,create,delete /usr/src/app/Frontend; do
-    echo "Changement détecté ! Redémarrage du service..."
-    npx tailwindcss -i ./Frontend/css/style.css -o ./Frontend/css/output.css
-done &
+# Dev
+# npm start &
 
-while inotifywait -r -e modify,create,delete /usr/src/app/Frontend/ts; do
-    echo "Changement détecté ! Redémarrage du service..."
-    npx tsc  # Ou relancer le processus concerné
-done &
+# while inotifywait -r -e modify,create,delete /usr/src/app/Frontend/templates; do
+#     echo "Changement détecté ! Redémarrage du service..."
+#     npx tailwindcss -i ./Frontend/css/style.css -o ./Frontend/css/output.css
+#     npx tsc  # Ou relancer le processus concerné
+# done &
 
-while inotifywait -r -e modify,create,delete /usr/src/app/Backend; do
-    echo "Changement détecté ! Redémarrage du serveur..."
-	kill $(pgrep -f "node")
-    npm start & # Ou relancer le processus concerné
-done
+# while inotifywait -r -e modify,create,delete /usr/src/app/Frontend; do
+#     echo "Changement détecté ! Redémarrage du service..."
+#     npx tailwindcss -i ./Frontend/css/style.css -o ./Frontend/css/output.css
+# done &
+
+# while inotifywait -r -e modify,create,delete /usr/src/app/Frontend/ts; do
+#     echo "Changement détecté ! Redémarrage du service..."
+#     npx tsc  # Ou relancer le processus concerné
+# done &
+
+# while inotifywait -r -e modify,create,delete /usr/src/app/Backend; do
+#     echo "Changement détecté ! Redémarrage du serveur..."
+# 	kill $(pgrep -f "node")
+#     npm start & # Ou relancer le processus concerné
+# done
